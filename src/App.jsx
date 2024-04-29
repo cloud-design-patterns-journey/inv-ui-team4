fa2e33967174ab528a46def4b82ec22446961671
Kuruyia <github@kuruyia.net>
2024-04-29 17:18:07 +0200

    Adds a mock service

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import UIShell from './content/UIShell/UIShell';
 import './App.scss';
+import { StockItemMockService } from "./services/stock-item-mock.service";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.stockService = props.stockService || new StockItemMockService();
+  }
+
   render() {
     return (
-      <div className="app">
-        <UIShell />
-      </div>
+        <div className="app">
+          <UIShell stockService={this.stockService} />
+        </div>
     );
   }
 }
